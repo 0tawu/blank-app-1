b9dfcf3689e7b6202de00f3d4a83af10bed5c580
0tawu <antoninlelostec@gmail.com>
2024-09-16 17:04:27 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,14 +22,9 @@ page = st.sidebar.selectbox(
 # Afficher la page sélectionnée
 if page == "Accueil":
     page_accueil()
+    
 elif page == "Search player":
     page_player()
-elif page == "Search team":
-    page_team()
-
-def get_so5_scores(player_slug):
-    url = 'https://api.sorare.com/federation/graphql'
-    
     query = '''
     {
         football {
@@ -49,15 +44,12 @@ def get_so5_scores(player_slug):
         }
     }
     '''
-
     options = {
         'json': {'query': query}
     }
-
     try:
         response = requests.post(url, **options)
         data = response.json()
-
         st.write("Données récupérées via l'API pour le joueur", player_slug, ":")
         if 'data' in data and data['data']['football']['player']['allSo5Scores']['nodes']:
             for node in data['data']['football']['player']['allSo5Scores']['nodes']:
@@ -69,7 +61,6 @@ def get_so5_scores(player_slug):
             st.write("Erreur: Aucune donnée valide récupérée pour le joueur", player_slug)
     except requests.exceptions.RequestException as e:
         st.write("Erreur lors de la requête à l'API:", e)
-
 # Afficher l'interface utilisateur
 st.title("Récupération des données pour un joueur sur Sorare")
 player_slug = st.text_input("Entrez le slug du joueur:")
@@ -78,3 +69,6 @@ if st.button("Récupérer les données"):
         get_so5_scores(player_slug)
     else:
         st.write("Veuillez entrer un slug de joueur pour récupérer les données.")
+elif page == "Search team":
+    
+    page_team()
